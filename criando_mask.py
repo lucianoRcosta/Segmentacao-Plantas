48457fb1ea57430015b7da27445cfd4339a0fe30
Luciano Ramos <thalmenoch@gmail.com>
2023-09-01 18:25:18 -0300

    ajuste de funções e organização de código

--- criando_mask.py
+++ criando_mask.py
@@ -1,33 +1,6 @@
 import cv2
 import os
-import numpy as np
-import matplotlib.pyplot as plt
-from clahe import Clahe
- 
-def contrast_check(img):
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-    histograma = cv2.calcHist([hsv], [2], None, [256], [0, 256])
-
-    dispersao = np.max(histograma) - np.min(histograma)
-    limite_dispersao = 14000
-
-    if dispersao >= limite_dispersao:
-        return Clahe(img)
-    else:
-        return img
-    
-#função temporária
-def maior_contorno(contornos):
-    maior_area = 0
-    maior_contorno = None
-    for contorno in contornos:
-        area = cv2.contourArea(contorno)
-        if area > maior_area:
-            maior_area = area
-            maior_contorno = contorno
-    return maior_contorno
-
+from functions import *
 
 if __name__ == '__main__':
     # img = cv2.imread('dataset/0a2de4c5-d688-4f9d-9107-ace1d281c307___Com.G_TgS_FL 7941_180deg.JPG')
@@ -35,12 +8,14 @@ if __name__ == '__main__':
     pasta_imgs = 'dataset/'
     arquivos = os.listdir(pasta_imgs)
 
+    #loop que pega os arquivos na pasta dataset
     for arq in arquivos:
-
+        #condição que checa se o arquivo termina com a extensão .jpg
         if arq.lower().endswith('jpg'):
             caminho_img = os.path.join(pasta_imgs, arq)
             img = cv2.imread(caminho_img)
 
+            #condição que checa se img não está vazio
             if img is not None:
                 #recebe a função de checagem de contraste
                 img_contrast_check = contrast_check(img)
@@ -51,45 +26,21 @@ if __name__ == '__main__':
                 #converte imagem para o espaço de cores hsv
                 hsv = cv2.cvtColor(borrado_mask, cv2.COLOR_BGR2HSV)
                 
-                #limite inferior e superior 1 de matiz de tonalidade verde
-                lower_green_1 = np.array([25, 20, 35])  
-                upper_green_1 = np.array([90, 255, 255])
-
-                mask1 = cv2.inRange(hsv, lower_green_1, upper_green_1)
-
-                #processo morfológico de fechamento
-                kernel = np.ones((3,3),np.uint8)
-                fechamento = cv2.morphologyEx(mask1, cv2.MORPH_CLOSE, kernel, iterations=2)   
+                mask = create_mask(hsv)
                 
-                #findContours sendo aplicada ao fechamento
-                contornos, _ = cv2.findContours(fechamento, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                mask_fechamento = fechamento(mask)  
                 
-                contorno_final = maior_contorno(contornos)
-                mascara_preta = np.zeros_like(img)
+                mask_contornos = find_draw_contours(mask_fechamento)
                 
-                #desenha contornos na máscara preta
-                cv2.drawContours(mascara_preta, [contorno_final], -1, (255, 255, 255), thickness=cv2.FILLED)
-
-                #processo morfológico de abertura
-                # kernel = np.ones((2,2),np.uint8)
-                # abertura = cv2.morphologyEx(fechamento, cv2.MORPH_OPEN, kernel, iterations=3)
-                
-                #FILTRO DE SOBEL
-                #Aplicação do filtro
-                sobelx = cv2.Sobel(fechamento, cv2.CV_64F, 1, 0, ksize=3)
-                sobely = cv2.Sobel(fechamento, cv2.CV_64F, 0, 1, ksize=3)
-                bordas = cv2.bitwise_or(np.absolute(sobelx), np.absolute(sobely))
-
-                #junção borda com a máscara, uso o and pq as dimensões de mask e bordas são diferentes
-                combinar = cv2.bitwise_and(bordas, bordas, mask=fechamento)
+                bordas, combinar = filtro_de_sobel(mask_contornos)
 
                 cv2.imshow('Imagem original', img)
-                cv2.imshow('Máscara da matiz', mask1)
+                cv2.imshow('Máscara da matiz', mask)
                 cv2.imshow('Bordas', bordas)
                 cv2.imshow('Junção da Máscara e Bordas', combinar)
                 # cv2.imshow('Abertura', abertura)
-                cv2.imshow('Fechamento', fechamento)
-                cv2.imshow('Máscara com Maior Contorno', mascara_preta)
+                cv2.imshow('Fechamento', mask_fechamento)
+                cv2.imshow('Maior Contorno', mask_contornos)
 
                 cv2.waitKey(0)
                 cv2.destroyAllWindows()
---
+++ functions.py
@@ -0,0 +1,100 @@
+import cv2
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+#cria a máscara com as matizes verdes
+def create_mask(img):
+    #limite inferior e superior 1 de matiz de tonalidade verde
+    lower_green_1 = np.array([25, 20, 35])  
+    upper_green_1 = np.array([90, 255, 255])
+
+    mask = cv2.inRange(img, lower_green_1, upper_green_1)
+
+    return mask
+
+#função de checagem de contraste para aplicação de clahe
+def contrast_check(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    histograma = cv2.calcHist([hsv], [2], None, [256], [0, 256])
+
+    dispersao = np.max(histograma) - np.min(histograma)
+    limite_dispersao = 12000
+
+    if dispersao >= limite_dispersao:
+        return clahe(img)
+    else:
+        return img
+
+def clahe(img):
+    #separação dos canais de cor da imagem
+    canal_azul, canal_verde, canal_vermelho = cv2.split(img)
+
+    #objeto clahe criado
+    clahe = cv2.createCLAHE(clipLimit=1.0, tileGridSize=(2, 2))
+
+    #clahe aplicado em cada canal
+    clahe_azul = clahe.apply(canal_azul)
+    clahe_verde = clahe.apply(canal_verde)
+    clahe_vermelho = clahe.apply(canal_vermelho)
+    
+    #junção dos canais com clahe aplicado
+    clahe_img = cv2.merge((clahe_azul, clahe_verde, clahe_vermelho))
+
+    return clahe_img
+
+#processo morfológico de fechamento
+def fechamento(img):
+    kernel = np.ones((3,3),np.uint8)
+    fechamento = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel, iterations=2) 
+
+    return fechamento
+
+#processo morfológico de abertura
+def abertura(img):
+    kernel = np.ones((2,2),np.uint8) # elemento estruturante
+    abertura = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel, iterations=3)
+
+    return abertura
+
+#função que encontra e desenha contornos
+def find_draw_contours(img):
+    #findContours sendo aplicada ao fechamento
+    contornos, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contorno_final = maior_contorno(contornos)
+    
+    #cria máscara preta
+    mascara_preta = np.zeros_like(img)
+    
+    #desenha contornos na máscara preta
+    cv2.drawContours(mascara_preta, [contorno_final], -1, (255, 255, 255), thickness=cv2.FILLED)
+
+    return mascara_preta
+    
+#função que encontra o maior contorno dentre a lista de contornos fornecida pela função findContours
+def maior_contorno(contornos):
+    maior_area = 0
+    maior_contorno = None
+
+    for contorno in contornos:
+        area = cv2.contourArea(contorno)
+    
+        if area > maior_area:
+            maior_area = area
+            maior_contorno = contorno
+    
+    return maior_contorno
+
+def filtro_de_sobel(img):
+    #Aplicação do filtro
+    sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
+    sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
+    
+    #operação or com os eixos x e y
+    bordas = cv2.bitwise_or(np.absolute(sobelx), np.absolute(sobely))
+
+    #junção borda com a máscara, uso do and porque as dimensões de mask e bordas são diferentes
+    combinar = cv2.bitwise_and(bordas, bordas, mask=img)
+
+    return bordas , combinar
